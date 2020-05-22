a8a5bdcc6cb2b175af82ad0ca21121ce71ff99ef
Jonathan 小林 Hales <jonathan@expo.io>
2020-05-22 06:38:28 -0700

    add Location.tsx

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { Compass } from "./components/MagnetometerSensor";
+import { LocationComponent } from "./components/Location";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Compass />
+      <LocationComponent />
     </View>
   );
 }
---
+++ components/Location.tsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import { Text, StyleSheet, View } from "react-native";
+import * as Location from "expo-location";
+
+export function LocationComponent() {
+  const [location, setLocation] = useState<Location.LocationData | null>(null);
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestPermissionsAsync();
+      if (status !== "granted") {
+        setErrorMsg("Permission to access location was denied");
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  });
+
+  let text = "Waiting..";
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>{text}</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
